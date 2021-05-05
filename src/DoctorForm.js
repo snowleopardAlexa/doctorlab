f4fa441ad8ccd0220d023e0a784950bba586349b
snowleopardAlexa <alexaslomski@gmail.com>
2021-05-04 20:19:36 -0400

    change Form bootstrap import

--- src/DoctorForm.js
+++ src/DoctorForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 
 function DoctorForm(props) {
     return (
       <div className="form">
-
+         <Form.Control type="..." placeholder="Enter email" />
       </div>
     );
 }
